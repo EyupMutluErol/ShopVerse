1fa5325ecf013e28239ce4e3787fc26dbe48759a
EyupMutluErol <eyupmutluerol@gmail.com>
2026-01-16 23:39:25 +0300

    Implement persistent Dark Mode and restore full footer layout

---
+++ ShopVerse.WebUI/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopVerse.WebUI.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            switch (statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "Üzgünüz, aradığınız sayfayı bulamadık.";
+                    // 404 için özel view döndür
+                    return View("NotFound");
+
+                    // İleride 500 veya 403 hataları için de case ekleyebilirsin.
+            }
+
+            return View("Error");
+        }
+    }
+}
